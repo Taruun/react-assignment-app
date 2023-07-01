5733c036e6e7169e3a22fe38ecf52fa13ad81840
Taruun <phantmetic@gmail.com>
2023-07-01 14:47:29 +0530

    added filterByDistance new filter

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -21,8 +21,10 @@ function List({
   rating,
 }) {
   const classes = useStyles();
-
+  const [distance, setDistance] = useState(4);
   const [elRefs, setElRefs] = useState([]);
+
+  const filteredPlaces = places?.filter((place) => place.distance <= distance);
 
   useEffect(() => {
     const refs = Array(places?.length)
@@ -43,15 +45,15 @@ function List({
       ) : (
         <>
           <FormControl className={classes.formControl}>
-            <InputLabel> Type</InputLabel>
+            <InputLabel>Type</InputLabel>
             <Select value={type} onChange={(e) => setType(e.target.value)}>
-              <MenuItem value="restaurants">Restraurants</MenuItem>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
               <MenuItem value="hotels">Hotels</MenuItem>
               <MenuItem value="attractions">Attractions</MenuItem>
             </Select>
           </FormControl>
           <FormControl className={classes.formControl}>
-            <InputLabel> Rating</InputLabel>
+            <InputLabel>Rating</InputLabel>
             <Select value={rating} onChange={(e) => setRating(e.target.value)}>
               <MenuItem value={0}>All</MenuItem>
               <MenuItem value={3}>Above 3.0</MenuItem>
@@ -59,13 +61,33 @@ function List({
               <MenuItem value={4.5}>Above 4.5</MenuItem>
             </Select>
           </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Distance (km)</InputLabel>
+            <Select
+              value={distance}
+              onChange={(e) => setDistance(e.target.value)}
+            >
+              <MenuItem value={0}>0 km</MenuItem>
+              <MenuItem value={1}>1 km</MenuItem>
+              <MenuItem value={2}>2 km</MenuItem>
+              <MenuItem value={3}>3 km</MenuItem>
+              <MenuItem value={4}>4 km</MenuItem>
+              <MenuItem value={5}>5 km</MenuItem>
+              <MenuItem value={6}>6 km</MenuItem>
+              <MenuItem value={7}>7 km</MenuItem>
+              <MenuItem value={8}>8 km</MenuItem>
+              <MenuItem value={9}>9 km</MenuItem>
+              <MenuItem value={10}>10 km</MenuItem>
+            </Select>
+          </FormControl>
           <Grid container spacing={3} className={classes.list}>
-            {places?.map((place, i) => (
+            {filteredPlaces?.map((place, i) => (
               <Grid ref={elRefs[i]} item key={i} xs={12}>
                 <PlaceDetails
                   place={place}
                   selected={Number(childClicked) === i}
                   refProp={elRefs[i]}
+                  distance={distance}
                 />
               </Grid>
             ))}
@@ -77,3 +99,4 @@ function List({
 }
 
 export default List;
+
